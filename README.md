2c40ac9a2d80cb2b8c10468445182cb4d467944b
SherlockHomer <406435735@qq.com>
2018-02-23 11:19:27 +0800

    changes in Lenovo

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Introduction :: it's copied from [AdminLTE.git](https://github.com/YOUR_USERNAME/AdminLTE.git)
+Introduction
+============
 
 ![Bower version](https://img.shields.io/bower/v/adminlte.svg)
 [![npm version](https://img.shields.io/npm/v/admin-lte.svg)](https://www.npmjs.com/package/admin-lte)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: './src/cmdb.js',
+    output: {
+        path: __dirname + '/build',
+        filename: "cmdb.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015', 'react', 'stage-2']
+            }
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    }
+    // plugins: [
+    //     new webpack.optimize.UglifyJsPlugin({
+    //         compress: {
+    //             warnings: false
+    //         }
+    //     })
+    // ]
+}
